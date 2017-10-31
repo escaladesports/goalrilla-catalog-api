e07b52007b52884dcf7eb35e4f4ca41223cc31ca
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-31 11:16:03 -0500

    Removed default comments

--- handler.js
+++ handler.js
@@ -10,9 +10,6 @@ module.exports.create = (event, context, callback) => {
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
 module.exports.fetch = (event, context, callback) => {
@@ -25,7 +22,4 @@ module.exports.fetch = (event, context, callback) => {
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
