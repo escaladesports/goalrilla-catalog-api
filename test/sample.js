078bca2af9e754682d22a48238c7f3a314ef277e
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-31 11:26:56 -0500

    Added sample test suite

---
+++ test/sample.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+
+// chai setup
+const should = chai.should();
+
+describe('Sample test', function() {
+	it('should pass', (done) => {
+		console.log('Sample test running');
+		done();
+	})
+})
