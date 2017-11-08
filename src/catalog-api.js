2584dbb86a975b494f4dcde87985cd6164328b33
Christian Sedlacek <csedlacek@escaladesports.com>
2017-11-08 15:27:54 -0600

    added function documentation for postCatalogRequest

--- src/catalog-api.js
+++ src/catalog-api.js
@@ -1,5 +1,17 @@
 const requestValidator = require('./request-validator.js');
 
+/**
+	Validates and handles catalog requests
+	@param {Object} params
+	@param {String} params.userFirstName
+	@param {String} params.userLastName
+	@param {String} params.userEmail
+	@param {String} params.userAddress
+	@param {String} params.userCity
+	@param {String} params.userState
+	@param {String} params.userZip
+	@returns {Promise.<Boolean|Error>} Returns a promise resolving to 'true' or rejecting with an error
+*/
 function postCatalogRequest(params) {
 	const err = requestValidator.validateCatalogRequest(params);
 	if (err !== true) {
