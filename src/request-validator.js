7e63c9794cf82c1354fa228be3dca634a0d3b3ac
Christian Sedlacek <csedlacek@escaladesports.com>
2017-11-08 11:10:52 -0600

    Catalog request validation behavior and tests implemented

--- src/request-validator.js
+++ src/request-validator.js
@@ -1,5 +1,31 @@
+const isEmail = require('isemail');
+const postcodeValidator = require('postcode-validator');
+
+function validateEmail(email) {
+	return isEmail.validate(email);
+}
+
+function validateZip(zip) {
+	const validAmerican = postcodeValidator.validate(zip, 'US');
+	const validCanadian = postcodeValidator.validate(zip, 'CA');
+
+	return (validAmerican || validCanadian);
+}
+
 function validateCatalogRequest(data) {
-	return false;
+	if (
+		!data || !data.userFirstName || !data.userLastName || !data.userEmail ||
+		!data.userAddress || !data.userCity || !data.userState || !data.userZip
+	) {
+		return false;
+	}
+	if (!validateEmail(data.userEmail)) {
+		return false;
+	}
+	if (!validateZip(data.userZip)) {
+		return false;
+	}
+	return true;
 }
 
 module.exports = {
--- test/request-validator.js
+++ test/request-validator.js
@@ -11,4 +11,109 @@ describe('Request Validator', function() {
 		expect(res).to.be.false;
 		done();
 	})
-})
+
+	it('should return false if required value is missing', done => {
+		const params = {
+			userFirstName: 'Test',
+			userLastName: 'Test',
+			// email missing
+			userAddress: '817 Maxwell Ave',
+			userCity: 'Evansville',
+			userState: 'Indiana',
+			userZip: '47711'
+		};
+		const res = reqValidator.validateCatalogRequest(params);
+		expect(res).to.be.false;
+		done();
+	});
+
+	it('should return false if email address is invalid', done => {
+		const params = {
+			userFirstName: 'Test',
+			userLastName: 'Test',
+			userEmail: 'NotCorrectlyFormatted',
+			userAddress: '817 Maxwell Ave',
+			userCity: 'Evansville',
+			userState: 'Indiana',
+			userZip: '47711'
+		};
+		const res = reqValidator.validateCatalogRequest(params);
+		expect(res).to.be.false;
+		done();
+	});
+
+	it('should return false if zip code is invalid American/Canadian zip code', done => {
+		const params = {
+			userFirstName: 'Test',
+			userLastName: 'Test',
+			userEmail: 'test@gmail.com',
+			userAddress: '817 Maxwell Ave',
+			userCity: 'Evansville',
+			userState: 'Indiana',
+			userZip: '123'
+		};
+		const res = reqValidator.validateCatalogRequest(params);
+		expect(res).to.be.false;
+		done();
+	});
+
+	it('should return true if zip code is valid American zip code', done => {
+		const params = {
+			userFirstName: 'Test',
+			userLastName: 'Test',
+			userEmail: 'test@gmail.com',
+			userAddress: '817 Maxwell Ave',
+			userCity: 'Evansville',
+			userState: 'Indiana',
+			userZip: '47711'
+		};
+		const res = reqValidator.validateCatalogRequest(params);
+		expect(res).to.be.true;
+		done();
+	});
+
+	it('should return true if zip code is valid Canadian zip code (formatted with a space)', done => {
+		const params = {
+			userFirstName: 'Test',
+			userLastName: 'Test',
+			userEmail: 'test@gmail.com',
+			userAddress: '817 Maxwell Ave',
+			userCity: 'Evansville',
+			userState: 'Indiana',
+			userZip: 'K1A 0A1'
+		};
+		const res = reqValidator.validateCatalogRequest(params);
+		expect(res).to.be.true;
+		done();
+	});
+
+	it('should return true if zip code is valid Canadian zip code (formatted without a space)', done => {
+		const params = {
+			userFirstName: 'Test',
+			userLastName: 'Test',
+			userEmail: 'test@gmail.com',
+			userAddress: '817 Maxwell Ave',
+			userCity: 'Evansville',
+			userState: 'Indiana',
+			userZip: 'K1A0A1'
+		};
+		const res = reqValidator.validateCatalogRequest(params);
+		expect(res).to.be.true;
+		done();
+	});
+
+	it('should return true if valid data is entered', done => {
+		const params = {
+			userFirstName: 'Test',
+			userLastName: 'Test',
+			userEmail: 'test@gmail.com',
+			userAddress: '817 Maxwell Ave',
+			userCity: 'Evansville',
+			userState: 'Indiana',
+			userZip: '47711'
+		};
+		const res = reqValidator.validateCatalogRequest(params);
+		expect(res).to.be.true;
+		done();
+	});
+});
