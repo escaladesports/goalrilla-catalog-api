c66617f9377866fe2a14e8fb44ed0c1aff0baa91
Christian Sedlacek <csedlacek@escaladesports.com>
2017-11-08 11:29:21 -0600

    Catalog request goes through validator and rejects with error if request data is invalid

--- src/catalog-api.js
+++ src/catalog-api.js
@@ -1,4 +1,9 @@
+const requestValidator = require('./request-validator.js');
+
 function postCatalogRequest(params) {
+	if (!requestValidator.validateCatalogRequest(params)) {
+		return Promise.reject(new Error('The request data was malformed- ensure parameter data is correct and try again'));
+	}
 	return Promise.resolve(true);
 }
 
