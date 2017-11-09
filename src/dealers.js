40147d3838c0ae3be871fa05f66de80a1a931151
Christian Sedlacek <csedlacek@escaladesports.com>
2017-11-09 16:21:17 -0600

    findNearestDealer now fetches dealer list

--- src/dealers.js
+++ src/dealers.js
@@ -1,12 +1,16 @@
 const request = require('request-promise-native');
 
+/**
+	Locates the nearest dealer based on a zip code
+	@param {String|Number} zip Zip code to search on
+	@returns {Promise.<Object>} Nearest dealer found
+*/
 function findNearestDealer(zip) {
-	/*const endpoint = `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/zip/${zip}/100`;
+	const endpoint = `https://apis.escaladesports.com/v1/dealers/territory/goalrilla/zip/${zip}/100`;
 	return request(endpoint).then(res => {
 		// sort
 		return res;
-	});*/
-	return Promise.resolve(['test']);
+	});
 }
 
 module.exports = {
