d44212d6fdabf5084e923f31d326f699c33f94fc
Christian Sedlacek <csedlacek@escaladesports.com>
2018-01-19 09:01:53 -0600

    Add store config

---
+++ config/store.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	production: {
+		spreadsheetId: '1AYkLlte3qTfc9OftFvdwcr3MalvFroxEZr59p3eFZ3E',
+		spreadsheetName: 'main'
+	}
+}
+
+// dev settings duplicate for now
+module.exports.dev = Object.assign({}, module.exports.production);
